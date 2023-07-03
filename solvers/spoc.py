bd6346edcde234cef034d72b9bf8e3b966bbeb65
Katia <katiachardon@outlook.fr>
2023-07-03 11:59:56 +0200

    spoc

--- solvers/spoc.py
+++ solvers/spoc.py
@@ -7,7 +7,11 @@ with safe_import_context() as import_ctx:
     import numpy as np
 
     # import your reusable functions here
-    from benchmark_utils import gradient_ols
+    import coffeine
+    from sklearn.pipeline import make_pipeline
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.linear_model import RidgeCV
+    from sklearn.feature_selection import VarianceThreshold
 
 
 # The benchmark solvers must be named `Solver` and
@@ -15,13 +19,23 @@ with safe_import_context() as import_ctx:
 class Solver(BaseSolver):
 
     # Name to select the solver in the CLI and to display the results.
-    name = 'GD'
+    name = 'Source Power Comodulation (SPoC)'
 
     # List of parameters for the solver. The benchmark will consider
     # the cross product for each key in the dictionary.
     # All parameters 'p' defined here are available as 'self.p'.
     parameters = {
-        'scale_step': [1, 1.99],
+        'rank' : ['full'],
+        'bands' : [{"low": (0.1, 1),
+            "delta": (1, 4),
+            "theta": (4.0, 8.0),
+            "alpha": (8.0, 15.0),
+            "beta_low": (15.0, 26.0),
+            "beta_mid": (26.0, 35.0),
+            "beta_high": (35.0, 49)
+        }],
+        'reg' : [1.e-05],
+        'scale' : ['auto']
     }
 
     def set_objective(self, X, y):
@@ -32,17 +46,28 @@ class Solver(BaseSolver):
         # It is customizable for each benchmark.
         self.X, self.y = X, y
 
-    def run(self, n_iter):
+        rank = self.rank
+        scale = self.scale
+        reg = self.reg
+        frequency_bands = self.bands
+
+        filter_bank_transformer = coffeine.make_filter_bank_transformer(
+            names=list(frequency_bands),
+            method='spoc',
+            projection_params=dict(scale=scale, n_compo=rank, reg=reg)
+        )
+        self.model = make_pipeline(
+            filter_bank_transformer,
+            VarianceThreshold(1e-10),
+            StandardScaler(),
+            RidgeCV(alphas=np.logspace(-5, 10, 100))
+        )
+
+    def run(self):
         # This is the function that is called to evaluate the solver.
         # It runs the algorithm for a given a number of iterations `n_iter`.
-
-        L = np.linalg.norm(self.X, ord=2) ** 2
-        alpha = self.scale_step / L
-        w = np.zeros(self.X.shape[1])
-        for _ in range(n_iter):
-            w -= alpha * gradient_ols(self.X, self.y, w)
-
-        self.w = w
+        
+        self.model.fit(self.X, self.y)
 
     def get_result(self):
         # Return the result from one optimization run.
